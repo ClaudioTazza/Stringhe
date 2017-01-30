d8d3197287ea38596034094502bfa275af8050a0
lucifero55 <claudiotazza@gmail.com>
2017-01-30 13:48:57 +0100

    Perche' da problemi?

---
+++ convert/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(){
+  char v[] = "243";
+  int num;
+
+  num = atoi(v);
+  printf("int: %d\n", num);
+
+  num = (double)num;
+  printf("double: %lf\n", num);
+
+  sprintf( v, "%d", num);
+  puts( v );
+
+  return 0;
+}
