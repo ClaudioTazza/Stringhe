1adb5dbc2b7659fccc46b719de1d95c9bb5fd463
lucifero55 <claudiotazza@gmail.com>
2017-02-01 12:37:54 +0100

    Done

--- concatena/main.c
+++ concatena/main.c
@@ -3,9 +3,9 @@
 #include <stdlib.h>
 
 int main(){
-  char v[] = "Ciao ", v2[] = "Mondo", v3[50] = ""; 
+  char v[] = "Ciao ", v2[] = "Mondo", v3[50]; 
 
-  strcat(v3, v);
+  strcpy(v3, v);
   strcat(v3, v2);
 
   printf("%s\n", v3);
